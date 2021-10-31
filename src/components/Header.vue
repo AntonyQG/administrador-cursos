535377b2907a43e44f99fbf60dee74c0d1d1881f
Antony Quintero <antonyquinterog@gmail.com>
2021-10-30 20:01:45 -0500

    Administrators table view without router

---
+++ src/components/Header.vue
@@ -0,0 +1,133 @@
+<template>
+  <v-app id="inspire" style="color: White">
+    <!-- Lateral Navigation -->
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item class="px-2">
+          <v-img
+            src="../assets/GLI.png"
+            style="
+               {
+                border: none;
+                margin: 0;
+                padding: 0;
+                max-width: 100%;
+                height: auto;
+              }
+            "
+          ></v-img>
+        </v-list-item>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item v-for="item in items" :key="item.title">
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+
+      <!-- Footer -->
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block>
+            ¿Necesitas Ayuda?
+            <v-icon>mdi-help-rhombus</v-icon>
+          </v-btn>
+        </div>
+      </template>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-card-text>
+    </v-navigation-drawer>
+
+    <!-- Header -->
+    <v-app-bar color="teal lighten-2" app>
+      <v-app-bar-nav-icon
+        @click="drawer = !drawer"
+        style="color: White"
+      ></v-app-bar-nav-icon>
+
+      <v-toolbar-title style="color: White">GLI </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- Icons -->
+      <v-btn icon style="color: White">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-divider vertical></v-divider>
+
+      <!-- User -->
+      <v-list
+        style="
+           {
+            border: none;
+            margin: 0;
+            padding: 0;
+            width: 14%;
+          }
+        "
+        color="teal lighten-2"
+      >
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img
+              src="https://randomuser.me/api/portraits/women/85.jpg"
+            ></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content style="color: White">
+            <v-list-item-title>Adriana Stefan</v-list-item-title>
+            <v-list-item-subtitle style="color: White"
+              >Recursos Humanos</v-list-item-subtitle
+            >
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-btn icon style="color: White">
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      group: null,
+      items: [
+        { title: "Administradores", icon: "mdi-shield-account" },
+        { title: "Administradores ", icon: "mdi-briefcase-account" },
+        { title: "Catálogos", icon: "mdi-notebook" },
+        { title: "Líderes", icon: "mdi-account-group" },
+        { title: "Usuarios", icon: "mdi-account-multiple" },
+        { title: "Mi Empresa", icon: "mdi-account-group-outline" },
+        { title: "Cursos", icon: "mdi-food-apple" },
+        { title: "Recompensas", icon: "mdi-shopping" },
+        { title: "Eventos", icon: "mdi-calendar-multiple" },
+        { title: "Notificaciones", icon: "mdi-comment-alert" },
+        { title: "Reportes", icon: "mdi-chart-timeline-variant" },
+      ],
+    };
+  },
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
---
+++ src/views/Administrators.vue
@@ -0,0 +1,266 @@
+<template>
+  <v-card>
+    <v-card-title primary-title> Adminostradores de la consola </v-card-title>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Buscar"
+        single-line
+        hide-details
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-btn
+        :loading="loading"
+        :disabled="loading"
+        color="blue-grey"
+        class="ma-2 white--text"
+        @click="loader = 'loading'"
+      >
+        Descargar
+        <v-icon right dark> mdi-download </v-icon>
+      </v-btn>
+      <v-btn
+        class="ma-2 white--text"
+        :loading="loading2"
+        :disabled="loading2"
+        color="teal lighten-2"
+        @click="loader = 'loading2'"
+      >
+        Añadir nuevo admin
+        <template v-slot:loader>
+          <span>Loading...</span>
+        </template>
+      </v-btn>
+    </v-card-title>
+    <v-data-table
+      v-model="selected"
+      show-select
+      item-key="admins"
+      :headers="headers"
+      :items="administradores"
+      :search="search"
+    >
+      <!-- Template for the Color of Estatus -->
+      <template v-slot:[`item.estatus`]="{ item }">
+        <v-chip :color="getColor(item.estatus)" dark>
+          {{ item.estatus }}
+        </v-chip>
+      </template>
+      <!-- Template for the Buttons -->
+      <template v-slot:[`item.detalles`]="{ item }">
+        <v-icon color="teal lighten-2" class="mr-2" @click="editItem(item)">
+          mdi-square-edit-outline
+        </v-icon>
+        <v-icon color="red accent-4" class="mr-2" @click="deleteItem(item)">
+          mdi-trash-can-outline
+        </v-icon>
+        <v-icon class="mr-2" @click="deleteItem(item)">
+          mdi-eye-outline
+        </v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="initialize"> Reset </v-btn>
+      </template>
+      <!-- Template for the Dialogs -->
+      <template v-slot:top>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5"
+              >¿Estás seguro que quieres eliminar a este
+              administrador?</v-card-title
+            >
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="red darken-1" text @click="closeDelete"
+                >Cancel</v-btn
+              >
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                >OK</v-btn
+              >
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <!-- Edit -->
+        <v-dialog v-model="dialogEdit" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5"
+              >¿Estás seguro que quieres editar este
+              administrador?</v-card-title
+            >
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="red darken-1" text @click="closeEdit">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="editItemConfirm"
+                >OK</v-btn
+              >
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  admins: "Administrators",
+  data() {
+    return {
+      search: "",
+      selected: [],
+      loader: null,
+      loading: false,
+      loading2: false,
+      dialogEdit: false,
+      dialogDelete: false,
+      editedIndex: -1,
+      editedItem: {
+        admins: "",
+        area: "",
+        email: "",
+        estatus: "",
+      },
+      headers: [
+        {
+          text: "Administradores",
+          align: "start",
+          sortable: false,
+          value: "admins",
+        },
+        { text: "Área", value: "area" },
+        { text: "Correo Electrónico", value: "email" },
+        { text: "Estatus", value: "estatus" },
+        { text: "Detalles", value: "detalles", sortable: false },
+      ],
+      administradores: [
+        {
+          admins: "Frozen Yogurt",
+          area: "Recursos Humanos",
+          email: "correo@gmail.com",
+          estatus: "Activo",
+          detalles: 4.0,
+        },
+        {
+          admins: "Ice cream sandwich",
+          area: "Recursos Humanos",
+          email: "correo@gmail.com",
+          estatus: "Inactivo",
+          detalles: 4.3,
+        },
+        {
+          admins: "Eclair",
+          area: "Recursos Humanos",
+          email: "correo@gmail.com",
+          estatus: "Activo",
+          detalles: 6.0,
+        },
+        {
+          admins: "Cupcake",
+          area: "Recursos Humanos",
+          email: "correo@gmail.com",
+          estatus: "Activo",
+          detalles: 4.3,
+        },
+        {
+          admins: "Gingerbread",
+          area: "Recursos Humanos",
+          email: "correo@gmail.com",
+          estatus: "Activo",
+          detalles: 3.9,
+        },
+        {
+          admins: "Jelly bean",
+          area: "Recursos Humanos",
+          email: "correo@gmail.com",
+          estatus: "Activo",
+          detalles: 0.0,
+        },
+        {
+          admins: "Lollipop",
+          area: "Recursos Humanos",
+          email: "correo@gmail.com",
+          estatus: "Activo",
+          detalles: 0,
+        },
+        {
+          admins: "Honeycomb",
+          area: "Recursos Humanos",
+          email: "correo@gmail.com",
+          estatus: "Inactivo",
+          detalles: 6.5,
+        },
+        {
+          admins: "Donut",
+          area: "Recursos Humanos",
+          email: "correo@gmail.com",
+          estatus: "Activo",
+          detalles: 4.9,
+        },
+        {
+          admins: "KitKat",
+          area: "Recursos Humanos",
+          email: "correo@gmail.com",
+          estatus: "Activo",
+          detalles: 7,
+        },
+      ],
+    };
+  },
+  methods: {
+    getColor(estatus) {
+      if (estatus == "Activo") return "green";
+      else return "red";
+    },
+    /*-- Methods for Edit --*/
+    // Modify
+    editItem(item) {
+      this.editedIndex = this.administradores.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogEdit = true;
+    },
+    editItemConfirm() {
+      this.desserts.splice(this.editedIndex, 1);
+      this.closeEdit();
+    },
+    closeEdit() {
+      this.dialogEdit = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+    /*-- Methods for Delete --*/
+    deleteItem(item) {
+      this.editedIndex = this.administradores.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+    deleteItemConfirm() {
+      this.desserts.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+  },
+  watch: {
+    loader() {
+      const l = this.loader;
+      this[l] = !this[l];
+
+      setTimeout(() => (this[l] = false), 3000);
+
+      this.loader = null;
+    },
+  },
+};
+</script>
+
+<style></style>
